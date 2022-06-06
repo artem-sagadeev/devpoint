9863ef021431a62c30830bc59c1327522481513b
unknown <sagadeev.artem2001@gmail.com>
2022-06-06 14:04:34 +0300

    add connections string

--- Domain.Subscriptions/Entities/SubscriptionLevel.cs
+++ Domain.Subscriptions/Entities/SubscriptionLevel.cs
@@ -4,6 +4,12 @@ public class SubscriptionLevel
 {
     public int Id { get; set; }
     public string Name { get; set; }
+
+    public SubscriptionLevel(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
     
     public SubscriptionLevel(string name)
     {
